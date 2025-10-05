4459a21a137b4e2aad2d7097e88e755b5ce338f8
Deep5656 <deepanshukatiyar5656@gmail.com>
2025-10-05 16:55:28 +0530

    routing issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { MenuComponent } from './shared-module/menu/menu.component';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { AppRoutingModule } from "./app-routing.module";
+import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from "../../node_modules/@angular/router/router_module.d";
 
 @Component({
   selector: 'app-root',
@@ -13,8 +15,8 @@ import { AppRoutingModule } from "./app-routing.module";
     CommonModule, // for ngIf, ngClass, ngFor
     HeaderComponent, // now <app-header> is known
     MenuComponent,
-    HomePageComponent,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterOutlet
 ],
   styleUrls: ['./app.component.scss']
 })
